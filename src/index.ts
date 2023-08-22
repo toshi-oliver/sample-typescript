ba10bffae21b233db31a7a66093e085ed78e954b
toshiakifurukawa <furukawa@binc.jp>
2023-08-22 12:04:16 +0900

    型アサーションを追加

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,10 @@ class HitAndBlow {
     private readonly answerSource = ['0','1','2','3','4','5','6','7','8','9']
     private answer : string[] = []
     private tryCount = 0
-    private mode: Mode
+    private mode: Mode = 'normal'
 
-    constructor(mode: Mode) {
-        this.mode = mode
-    }
-
-    public setting() {
+    async setting() {
+        this.mode = await promptInput('モードを入力してください。') as Mode
         const answerLength = this.getAnswerLength()
 
         while (this.answer.length < answerLength) {
@@ -101,7 +98,8 @@ const promptInput = async (text: string) => {
 }
 
 (async ()  => {
-    const hitAndBlow = new HitAndBlow('hard')
+    const hitAndBlow = new HitAndBlow()
+    await hitAndBlow.setting()
     await hitAndBlow.play()
     hitAndBlow.end()
 })()
